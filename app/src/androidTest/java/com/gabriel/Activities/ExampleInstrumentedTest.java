8abd73f72699f9b086b72ce8d41464f9ffffcb21
Gabriel Samartin <gabrielsamartin2004@gmail.com>
2025-11-13 19:46:30 -0300

    Commit inicial - Tela 1 funcionando

--- app/src/androidTest/java/com/gabriel/Activities/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/gabriel/Activities/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.gabriel.barberapp", appContext.getPackageName());
+        assertEquals("com.gabriel.Activities", appContext.getPackageName());
     }
 }
